354557edbd4777a1c88b6cbd3976c2c9de3897cc
João Fonseca <joao.fonseca@ua.pt>
2023-10-18 16:59:53 +0100

    remove preview

--- app/src/main/java/cm/project/android/projectx/MainActivity.kt
+++ app/src/main/java/cm/project/android/projectx/MainActivity.kt
@@ -9,15 +9,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import cm.project.android.projectx.ui.App
 import cm.project.android.projectx.ui.theme.ProjectXTheme
 import com.google.firebase.FirebaseApp
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonBuilder
 import org.osmdroid.config.Configuration
 
 
@@ -46,21 +41,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjectXTheme {
-        Greeting("Android")
     }
 }
